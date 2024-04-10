1fdfa7aed476b470eaabd5da89190247319e754e
Jenutka <honza.prokes@gmail.com>
2024-04-10 22:26:04 +0200

    completed class

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ class NeuralNetwork():
 
             output = self.think(training_inputs)
             error = training_outputs - output
-            adjustments = np.dot(training_inputs.T, error * \
-                    self.sigmoid_derivative(output))
+            adjustments = np.dot(training_inputs.T, error \
+                                * self.sigmoid_derivative(output))
             self.synaptic_weights += adjustments
 
     def think(self, inputs):
@@ -30,3 +30,31 @@ class NeuralNetwork():
         output = self.sigmoid(np.dot(inputs, self.synaptic_weights))
 
         return output
+
+
+if __name__ == "__main__":
+
+    neural_network = NeuralNetwork()
+
+    print("Random synaptic weights: ")
+    print(neural_network.synaptic_weights)
+
+    training_inputs = np.array([[0,0,1],
+                                [1,1,1],
+                                [1,0,1],
+                                [0,1,1]])
+
+    training_outputs = np.array([[0,1,1,0]]).T
+
+    neural_network.train(training_inputs, training_outputs, 10000)
+
+    print("Synaptic weights after training: ")
+    print(neural_network.synaptic_weights)
+
+    A = str(input("Input 1: "))
+    B = str(input("Input 2: "))
+    C = str(input("Input 3: "))
+
+    print("New situation: input data = ", A, B, C)
+    print("Output data: ")
+    print(neural_network.think(np.array([A, B, C])))
--- neural_network_01.py
+++ neural_network_01.py
@@ -1,4 +1,4 @@
-import numpy as np
+mport numpy as np
 
 
 def sigmoid(x):
